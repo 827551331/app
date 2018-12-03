d48adeec07ab2df6dab7b3f29654b6477f15cfb3
wang_yw <wdssdream@163.com>
2018-12-04 00:20:07 +0800

    .
    .

---
+++ src/main/java/com/ws/app/entity/AppController.java
@@ -0,0 +1,15 @@
+package com.ws.app.entity;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/app")
+public class AppController {
+
+    @RequestMapping(value = "/login")
+    public String hello() {
+        return "test file";
+    }
+}
