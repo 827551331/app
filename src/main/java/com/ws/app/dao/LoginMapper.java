98f144549f6ac2c284b26434a06e8665485805a5
wang_yw <wdssdream@163.com>
2018-12-10 07:52:55 +0800

    启动项目
    启动项目

--- src/main/java/com/ws/app/dao/LoginMapper.java
+++ src/main/java/com/ws/app/dao/LoginMapper.java
@@ -3,10 +3,11 @@ package com.ws.app.dao;
 
 import org.apache.ibatis.annotations.Select;
 
-public class LoginMapper {
+import java.util.List;
+import java.util.Map;
 
-    @Select("SELECT 操作员编码 AS userid,姓名 AS username FROM T操作人员 where 姓名='吴玲'")
-    public String login() {
-        return "test file";
-    }
+public interface LoginMapper {
+
+    @Select({"SELECT 操作员编码 AS userid,姓名 AS username FROM T操作人员 where 姓名='吴玲'"})
+    public List<Map<String,Object>> login();
 }
