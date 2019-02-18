6d08f5d45d012468ec6d975249a2f52ca38cdde1
wangyiwei <wdssdream@163.com>
2019-02-18 17:51:02 +0800

    update 下载表册信息
    update表册信息，增加用户用水信息中表册ID字段

--- src/main/java/com/ws/app/dao/LoginMapper.java
+++ src/main/java/com/ws/app/dao/LoginMapper.java
@@ -28,13 +28,19 @@ public interface LoginMapper {
      * @param userNo
      * @return
      */
-    @Select({"SELECT  A.客户编码 AS consNo,A.抄表线路 AS bcId,B.线路名称 bcName,A.水表位置 sbAddr,B.CBY userName,C.操作员编码 userNo FROM T客户水表 A INNER JOIN T抄表线路 B " +
-            " ON A.抄表线路 = B.线路编码 AND A.部门编码 = B.营业所编码 " +
-            " INNER JOIN T操作人员 C ON B.CBY = C.姓名 " +
-            " WHERE A.供水情况 = '0' " +
-            " AND C.操作员编码 = #{userNo} " +
-            " GROUP BY A.客户编码,B.线路名称,A.水表位置,B.CBY,C.操作员编码,A.抄表线路 " +
-            " ORDER BY B.线路名称,A.水表位置"})
+//    @Select({"SELECT  A.客户编码 AS consNo,A.抄表线路 AS bcId,B.线路名称 bcName,A.水表位置 sbAddr,B.CBY userName,C.操作员编码 userNo FROM T客户水表 A INNER JOIN T抄表线路 B " +
+//            " ON A.抄表线路 = B.线路编码 AND A.部门编码 = B.营业所编码 " +
+//            " INNER JOIN T操作人员 C ON B.CBY = C.姓名 " +
+//            " WHERE A.供水情况 = '0' " +
+//            " AND C.操作员编码 = #{userNo} " +
+//            " GROUP BY A.客户编码,B.线路名称,A.水表位置,B.CBY,C.操作员编码,A.抄表线路 " +
+//            " ORDER BY B.线路名称,A.水表位置"})
+    @Select({"SELECT B.客户编码 consNo,MAX(C.客户名称) AS consName , MAX(B.祥细地址) AS sbAddr,A.线路名称 bcName,B.水表位置 sbSite,A.线路编码 bcId FROM T抄表线路 A INNER JOIN  t客户水表 B " +
+            " ON A.线路编码 = B.抄表线路 INNER JOIN  T客户资料 C ON  B.客户编码 = C.客户编码 " +
+            " AND A.营业所编码 = B.部门编码 INNER JOIN T操作人员 D ON A.CBY = D.姓名 " +
+            " WHERE B.供水情况 = '0' and D.操作员编码 = #{userNo} " +
+            " GROUP BY A.线路名称,B.水表位置,B.客户编码,A.线路编码 " +
+            " ORDER BY A.线路名称 "})
     public List<Map<String, Object>> getBCInfo(String userNo);
 
 
@@ -44,20 +50,14 @@ public interface LoginMapper {
      * @param consNo
      * @return
      */
-//    @Select({"SELECT A.客户编码 consNo,A.上月表数 startCode,A.本月表数 endCode,A.实用水量 num,B.年月 dateYM FROM t用水记录 A " +
-//            " INNER JOIN  T用水年月 B ON A.用水年月 = B.年月编码 " +
-//            " WHERE A.客户编码 = #{consNo} " +
-//            " AND A.用水年月 BETWEEN '169' AND '179' " +
-//            " GROUP BY A.客户编码,A.上月表数,A.本月表数,A.实用水量,B.年月 " +
-//            " ORDER BY B.年月"})
-    @Select({"SELECT DISTINCT(A.客户编码) consNo,MAX(A.客户名称) AS consName,MAX(B.祥细地址) AS addr,MAX(A.联系电话) phone,C.上月表数 startCode,C.本月表数 endCode,D.年月 dateYM FROM T客户资料 A INNER JOIN T客户水表 B " +
+    @Select({"SELECT DISTINCT(A.客户编码) consNo,MAX(A.客户名称) AS consName,MAX(B.祥细地址) AS addr,MAX(A.联系电话) phone,C.上月表数 startCode,C.本月表数 endCode,D.年月 dateYM,B.抄表线路 bdId FROM T客户资料 A INNER JOIN T客户水表 B " +
             " ON A.客户编码 = B.客户编码 " +
             " INNER JOIN T用水记录 C ON A.客户编码 = C.客户编码 AND A.客户编码 = C.客户编码 INNER JOIN T用水年月 D ON C.用水年月 = D.年月编码  " +
             " WHERE A.客户编码 = #{consNo} " +
             " AND D.年月 BETWEEN #{startDate} AND #{endDate} " +
-            " GROUP BY A.客户编码,D.年月,C.上月表数,C.本月表数,A.客户名称 " +
+            " GROUP BY A.客户编码,D.年月,C.上月表数,C.本月表数,A.客户名称,B.抄表线路 " +
             " ORDER BY D.年月 "})
-    public List<Map<String, Object>> getWaterInfo(String consNo,String startDate,String endDate);
+    public List<Map<String, Object>> getWaterInfo(String consNo, String startDate, String endDate);
 
     /**
      * 上传止码
